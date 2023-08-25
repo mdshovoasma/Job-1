d8fc79d4003ccb13b975a89ecb6de3b75648bdfc
Md shovo677 <97186751+mdshovoasma@users.noreply.github.com>
2023-08-25 17:03:33 +0600

    Add files via upload

---
+++ Job-1.php
@@ -0,0 +1,14 @@
+<?Php 
+// string concatenation
+echo "My name is shovo"." i am 20 years old.<br><br><br>";
+//variable declaration
+
+$name = "shovo";
+$age = 20;
+ //output of variable in dbl cotation
+ echo "My name is $name i am $age Years  old .<br> <br><br>";
+//output of variable in single cotation
+ echo 'My name is '.$name.' I am '.$age.' years  old';
+
+
+?>
